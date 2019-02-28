096158b63760135d8e3d6421c67e81621ce506c7
Benjamin Lannon <benjamin@lannonbr.com>
2019-02-28 17:51:56 -0500

    Initial prep for deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,15 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hello!</h1>
+    <p>
+      This site will be a place where I can use to reference resources that I
+      reccomend to others.
+    </p>
+    <p>
+      If you want to head to my current site, head to{" "}
+      <a href="https://lannonbr.com">lannonbr.com</a>
+    </p>
   </Layout>
 )
 
