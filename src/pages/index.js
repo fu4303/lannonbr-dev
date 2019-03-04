395158cc402f6a4de3f0c61146a0ea1e1ade6184
Benjamin Lannon <benjamin@lannonbr.com>
2019-03-03 21:58:57 -0500

    adding illustration to frontpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,25 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import Folder from "../images/folder.svg"
+
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hello!</h1>
-    <p>
-      This site will be a place where I can use to reference resources that I
-      reccomend to others.
-    </p>
+    <div className="splitBox">
+      <div
+        style={{
+          paddingRight: 20,
+        }}
+      >
+        <h1>Hello!</h1>
+        <p>
+          This site will be a place where I can use to reference resources that
+          I reccomend to others.
+        </p>
+      </div>
+      <img src={Folder} alt={`man standing near folder`} />
+    </div>
     <p>
       If you want to head to my current site, head to{" "}
       <a href="https://lannonbr.com">lannonbr.com</a>
