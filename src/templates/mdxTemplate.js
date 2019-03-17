b4d060809410539b6109f8e9384a00307c1d1209
Benjamin Lannon <benjamin@lannonbr.com>
2019-03-16 23:49:17 -0400

    Add link to github source for each content page
    This now allows for quick access to the sources, so if someone sees a typo, they can quickly
    go over to GitHub and then make the change.
    
    Resolves #11 on GitHub

--- src/templates/mdxTemplate.js
+++ src/templates/mdxTemplate.js
@@ -2,13 +2,25 @@ import React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-mdx"
 import SEO from "../components/seo"
+import EditSVG from "../images/edit.svg"
 
 const MDXTemplate = ({ data }) => {
+  let fileName = data.mdx.fileAbsolutePath.split("/").pop()
+
   return (
     <>
       <SEO title={data.mdx.frontmatter.title} />
       <h1>{data.mdx.frontmatter.title}</h1>
       <MDXRenderer>{data.mdx.code.body}</MDXRenderer>
+      <p style={{ display: "flex", justifyContent: "flex-end" }}>
+        <a
+          href={`https://github.com/lannonbr/lannonbr.dev/blob/master/src/content/${fileName}`}
+          style={{ display: "inline-flex", fontSize: 16 }}
+        >
+          <img src={EditSVG} alt="Edit SVG" />
+          Edit this page on GitHub
+        </a>
+      </p>
     </>
   )
 }
@@ -19,6 +31,7 @@ export const PageQuery = graphql`
       code {
         body
       }
+      fileAbsolutePath
       frontmatter {
         title
       }
