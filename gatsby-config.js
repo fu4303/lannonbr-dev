98a63f6bbe894fb195b028134a1a54ca3fc30fad
Benjamin Lannon <benjamin@lannonbr.com>
2019-10-04 23:47:08 -0400

    Updating dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  __experimentalThemes: [
-    {
-      resolve: `gatsby-theme-lannonbr`,
-      options: {},
-    },
-  ],
   siteMetadata: {
     title: `Lannonbr.dev`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -12,6 +6,7 @@ module.exports = {
     navigationLinks: [{ url: "/", name: "Home" }],
   },
   plugins: [
+    `gatsby-theme-lannonbr`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -32,6 +27,19 @@ module.exports = {
       options: {
         extensions: [`.md`, `.mdx`],
         gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1000,
+            },
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {},
+          },
+        ],
+        // Remove when the 'plugins' key is not special anymore
+        plugins: [
           {
             resolve: `gatsby-remark-images`,
             options: {
