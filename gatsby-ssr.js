df866a0771032c4bcf58950fa90f3065b2f7c73f
Benjamin Lannon <benjamin@lannonbr.com>
2020-10-07 08:39:51 -0400

    Add algolia clientside code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,21 @@
  */
 
 // You can delete this file if you're not using it
+const React = require('react')
+
+exports.onRenderBody = function({ setPreBodyComponents }) {
+  setPreBodyComponents([
+    React.createElement('link', { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@0/dist/algoliasearchNetlify.css" }),
+    React.createElement('script', { type: "text/javascript", src: "https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@0/dist/algoliasearchNetlify.js" }),
+    React.createElement('script', { type: "text/javascript", dangerouslySetInnerHTML: {
+      __html: `
+      algoliasearchNetlify({
+        appId: 'GZ7JSDZAEK',
+        apiKey: '27011eaa070119a57682636fd78b274a',
+        siteId: 'dac6abff-36f5-4149-ade2-085c639c86a8',
+        branch: 'master',
+      });
+      `
+    } })
+  ])
+}
